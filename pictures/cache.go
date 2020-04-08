359c63187224c383d686a55ed8cee07ffc9731e9
mbraeutig <34983234+mbraeutig@users.noreply.github.com>
2020-04-08 20:30:41 +0200

    ...

--- pictures/cache.go
+++ pictures/cache.go
@@ -3,13 +3,22 @@ package pictures
 import (
 	"bytes"
 	"io"
+	"io/ioutil"
 	"log"
 	"os"
 )
 
 var Gopher bytes.Buffer
+var Test *bytes.Reader
 
 func init() {
+
+	b, err := ioutil.ReadFile("./pictures/gophercolor.png")
+	if err != nil {
+		log.Fatal(err)
+	}
+	Test = bytes.NewReader(b)
+
 	f, err := os.Open("./pictures/gophercolor.png")
 	if err != nil {
 		log.Fatal(err)
@@ -19,5 +28,4 @@ func init() {
 	if _, err := io.Copy(&Gopher, f); err != nil {
 		log.Fatal(err)
 	}
-
 }
