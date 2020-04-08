72240949cca1fc7531b74dbcbc116e4ae94648e0
mbraeutig <34983234+mbraeutig@users.noreply.github.com>
2020-04-08 18:22:05 +0200

    Update gopher.go

--- api/gopher.go
+++ api/gopher.go
@@ -3,12 +3,23 @@ package api
 import (
 	"fmt"
 	"io"
+	"io/ioutil"
 	"net/http"
 	"os"
 )
 
 // Gopher prints a gopher.
 func Gopher(w http.ResponseWriter, r *http.Request) {
+
+	infos, err := ioutil.ReadDir(".")
+	if err != nil {
+		http.Error(w, fmt.Sprintf("Error reading dir: %v", err), http.StatusInternalServerError)
+		return
+	}
+	for _, i := range infos {
+		fmt.Print(w, i.Name())
+	}
+
 	// Read the gopher image file.
 	f, err := os.Open("gophercolor.png")
 	if err != nil {
