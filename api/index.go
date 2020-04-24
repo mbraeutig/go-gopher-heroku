a201bcb2036533a4563baac0d6997c98de1a955f
mbraeutig <34983234+mbraeutig@users.noreply.github.com>
2020-04-24 20:30:14 +0200

    Update index.go

--- api/index.go
+++ api/index.go
@@ -8,11 +8,13 @@ import (
 
 const index = "" +
 	`
+<head>	
 <html>
-	<link rel="shortcut icon" type="image/x-icon" href="images/favicon.ico">
+	<link rel="shortcut icon" type="image/x-icon" href="./images/favicon.ico">
 	<h1>Want to see a gopher, try this!</h1>
 	<h1><a href="gopher">Gopher</a></h1>
 </html>
+</head>
 `
 
 func Index(w http.ResponseWriter, r *http.Request) {
